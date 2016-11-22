cbe7c013477418492bf8e3e1551ff15950cd3555
Brandon <Brandon@CHUCK>
2016-11-22 09:53:41 -0600

    test branch

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("TEST");
+            Console.WriteLine("TESTS");
         }
     }
 }
